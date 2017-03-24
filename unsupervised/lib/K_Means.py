fbc262755e273157d2a8c57de8ded5c285a28fba
singhShubh <shubham.ece@outlook.com>
2017-03-24 16:15:08 +0530

    2nd commit

--- unsupervised/lib/K_Means.py
+++ unsupervised/lib/K_Means.py
@@ -2,23 +2,38 @@ import numpy as np
 from sklearn.cluster import KMeans
 from matplotlib import pyplot as plt
 
-
-
 '''
     Creating dataset for clustering
 '''
-
-plt.style.use('ggplot')
 data = np.array([[1,3],[3,2],[4,4],[8,8],[9,7],[7,10]])
+plt.style.use('ggplot')
+plt.title('Data')
+plt.scatter(data[:,0],data[:,1])
+plt.show()
 
+'''
+    Creating the object of KMeans
+'''
+model = KMeans(n_clusters=2)
+model.fit(X=data)
+cluster_centers = model.cluster_centers_
+labels = model.labels_
 
-clf = KMeans(n_clusters=2)
-clf.fit(X=data)
-cluster_centers=clf.cluster_centers_
-labels=clf.labels_
-label_color = ["r.","g."]
-
+'''
+    Displaying the clusters annd the cluster centers
+'''
+label_color = ["r","g"]
 for i in range(len(data)):
-    plt.plot(data[i][0],data[i][1],label_color[labels[i]], markersize=10)
-plt.scatter(cluster_centers[:,0], cluster_centers[:,1],s=10,marker='x',linewidths=10)
+    plt.plot(data[i][0],data[i][1],label_color[labels[i]],marker='.',markersize=10)
+plt.scatter(cluster_centers[:,0],cluster_centers[:,1],marker='x',s=5,linewidth=10)
+plt.show()
+
+'''
+    Now we will try to classify a new datapoint
+'''
+x = [[2,5]]
+label = model.predict(x)
+for i in range(len(data)):
+    plt.plot(data[i][0],data[i][1],label_color[labels[i]],marker='.',markersize=10)
+plt.scatter(x[0][0],x[0][1],c=label_color[label[0]],s=5,marker='x', linewidth=10)
 plt.show()
